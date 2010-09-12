028a0a0181fe5572308bcddf3345b53cd74e3888
Xedecimal <devnull@localhost>
2010-09-12 00:56:03 +0000

    Added a loading progress indicator for running fixes.

--- modules/check/_check.js
+++ modules/check/_check.js
@@ -1,6 +1,7 @@
 $(function () {
 	$('.a-fix').click(function () {
 		$(this).load($(this).attr('href'));
+		$(this).attr('href', '').html('<img src="img/load.gif" alt="loading" />')
 		return false;
 	});
 	
