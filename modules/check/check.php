42a522bc50640a2fd04e2b387d0dee438db18319
Xedecimal <Xedecimal@gmail.com>
2010-10-17 13:51:06 -0700

    = General =  * Fixed some navigation links for the new navigation system.

--- modules/check/check.php
+++ modules/check/check.php
@@ -10,7 +10,7 @@ class ModCheck extends Module
 
 		if (empty($_d['q'][0]))
 		{
-			$_d['nav.links']->AddChild(new TreeNode('Check your library', 'check'));
+			$_d['nav.links']['Check Your Library'] = 'check';
 			$_d['head'] .= '<link type="text/css" rel="stylesheet" href="modules/check/css.css" />';
 			#return '<a href="check" id="a-check"></a>';
 		}
