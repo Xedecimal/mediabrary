7a7d9c2b368a372d57335e5bb2c2f85fdaa6d9e6
Xedecimal <Xedecimal@gmail.com>
2010-10-17 22:53:17 -0700

    = Seriesly =  * Added support for this service, very awesome stuff!

--- modules/seriesly.php
+++ modules/seriesly.php
@@ -2,12 +2,33 @@
 
 class ModSeriesly extends Module
 {
+	function __construct()
+	{
+		$this->CheckActive('seriesly');
+	}
+
 	function Get()
 	{
 		if (empty($_d['q'][0]))
 		{
-			return 'For Seriesly support Add the following web hook: ';
+			return "For Seriesly support Add the following web hook: http://{$_SERVER['HTTP_HOST']}{{app_abs}}/seriesly";
 		}
+	}
+
+	function Prepare()
+	{
+		if (!$this->Active) return;
+		$xml = file_get_contents('php://input');
+		//$xml = file_get_contents('seriesly2.txt');
+		$sx = simplexml_load_string($xml);
+		foreach ($sx->xpath('//release[quality="HDTV"]/url') as $url)
+		{
+			$fname = basename($url);
+			file_put_contents("/data/nas/torrent-files/$fname",
+				file_get_contents($url));
+			//varinfo((string)$url);
+		}
+		die();
 	}
 }
 
