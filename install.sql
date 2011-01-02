6f5f246c1747802328adc165d50f6589bf4b9485
Xedecimal <Xedecimal@gmail.com>
2011-01-01 23:08:02 -0800

    Fixed up a lot of sql, with some foreign keys and better table types.

--- install.sql
+++ install.sql
@@ -1,9 +1,9 @@
 # --------------------------------------------------------
-# Host:                         192.168.1.2
-# Server version:               5.1.52
-# Server OS:                    redhat-linux-gnu
+# Host:                         127.0.0.1
+# Server version:               5.1.31-community-log
+# Server OS:                    Win32
 # HeidiSQL version:             6.0.0.3627
-# Date/time:                    2011-01-01 20:04:28
+# Date/time:                    2011-01-01 23:07:00
 # --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS `codec` (
   `cod_name` varchar(255) NOT NULL,
   `cod_value` varchar(255) NOT NULL,
   PRIMARY KEY (`cod_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 # Data exporting was unselected.
 
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS `movie` (
   `mov_date` date NOT NULL,
   `mov_tmdbid` int(10) unsigned NOT NULL,
   PRIMARY KEY (`mov_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 # Data exporting was unselected.
 
@@ -48,8 +48,9 @@ CREATE TABLE IF NOT EXISTS `movie_category` (
   `cat_movie` int(10) unsigned NOT NULL,
   `cat_name` varchar(255) NOT NULL,
   PRIMARY KEY (`cat_id`),
-  UNIQUE KEY `cat_movie` (`cat_movie`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  UNIQUE KEY `cat_movie` (`cat_movie`),
+  CONSTRAINT `FK_movie_category_movie` FOREIGN KEY (`cat_movie`) REFERENCES `movie` (`mov_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 # Data exporting was unselected.
 
@@ -60,8 +61,10 @@ CREATE TABLE IF NOT EXISTS `movie_date` (
   `md_movie` int(10) unsigned NOT NULL DEFAULT '0',
   `md_name` varchar(255) NOT NULL,
   `md_date` date NOT NULL,
-  PRIMARY KEY (`md_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`md_id`),
+  KEY `FK_movie_date_movie` (`md_movie`),
+  CONSTRAINT `FK_movie_date_movie` FOREIGN KEY (`md_movie`) REFERENCES `movie` (`mov_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 # Data exporting was unselected.
 
@@ -72,8 +75,10 @@ CREATE TABLE IF NOT EXISTS `movie_detail` (
   `md_movie` int(10) unsigned NOT NULL DEFAULT '0',
   `md_name` varchar(255) NOT NULL DEFAULT '0',
   `md_value` varchar(255) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`md_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`md_id`),
+  KEY `FK_movie_detail_movie` (`md_movie`),
+  CONSTRAINT `FK_movie_detail_movie` FOREIGN KEY (`md_movie`) REFERENCES `movie` (`mov_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 # Data exporting was unselected.
 
@@ -84,8 +89,10 @@ CREATE TABLE IF NOT EXISTS `movie_float` (
   `mf_movie` int(10) unsigned NOT NULL,
   `mf_name` varchar(255) NOT NULL,
   `mf_value` float NOT NULL,
-  PRIMARY KEY (`mf_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`mf_id`),
+  KEY `FK_movie_float_movie` (`mf_movie`),
+  CONSTRAINT `FK_movie_float_movie` FOREIGN KEY (`mf_movie`) REFERENCES `movie` (`mov_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 # Data exporting was unselected.
 
@@ -93,11 +100,12 @@ CREATE TABLE IF NOT EXISTS `movie_float` (
 # Dumping structure for table mediabrary.rate
 CREATE TABLE IF NOT EXISTS `rate` (
   `rate_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `rate_for` int(10) NOT NULL,
+  `rate_for` int(10) unsigned NOT NULL,
   `rate_from` int(10) unsigned NOT NULL,
   PRIMARY KEY (`rate_id`),
-  UNIQUE KEY `rate_for` (`rate_for`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  UNIQUE KEY `rate_for` (`rate_for`),
+  CONSTRAINT `FK_rate_movie` FOREIGN KEY (`rate_for`) REFERENCES `movie` (`mov_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 # Data exporting was unselected.
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
