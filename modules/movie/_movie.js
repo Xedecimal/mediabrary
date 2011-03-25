05e8b805b1efb419f39767a86a240062af448a34
Xedecimal <Xedecimal@gmail.com>
2011-03-25 01:40:44 -0700

    iPad/iPhone click fix! And a bit of cleanup.

--- modules/movie/_movie.js
+++ modules/movie/_movie.js
@@ -20,24 +20,12 @@ $(function () {
 	});
 
 	$('.a-movie-item').live('click', function () {
-		if ($(this).find('img').attr('src') == 'modules/movie/img/loading.jpg')
-		{
-			alert('Please wait until this item is done loading, multiple \
-				requests will most likely cause you headaches.');
-			return false;
-		}
-
 		$('#dialog-movie').dialog('option', 'title',
 			'Details for '+$(this).attr('href'));
-
-		/*$('#movie-items a').css('opacity', 1);
-		$('#movie-items a[href!='+$(this).attr('href')+']').css('opacity', 0.15);*/
 
 		$('#dialog-movie').load(app_abs+'/movie/detail',
 			{path: $(this).attr('href')}, function () {
 				$('#dialog-movie').dialog('open');
-				//img = $('input[name="movie-bd"]').val();
-				//$('.ui-widget-overlay').css('background', 'url(\''+img+'\') no-repeat center');
 			}
 		);
 
