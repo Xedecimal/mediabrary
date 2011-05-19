7ca598dbd9a85ad91f7fa0b04037fe583e68fc5f
Xedecimal <Xedecimal@gmail.com>
2011-05-18 23:55:40 -0700

    Added filesystem scraping support to MediaEntry.

--- classes/media_entry.php
+++ classes/media_entry.php
@@ -9,10 +9,26 @@ class MediaEntry
 	# Metadata
 	public $Title;
 	
-	function __construct($path)
+	function __construct($path, $parses = null)
 	{
 		$this->Path = $path;
-		$this->Title = $this->Filename = basename($path);
+		$this->Filename = basename($path);
+
+		if (!empty($parses))
+		{
+			$mx = 0;
+			foreach ($parses as $preg => $matches)
+			{
+				if (preg_match($preg, $path, $m))
+				{
+					foreach ($matches as $idx => $col) $this->$col = $m[$idx];
+					$this->DebugMatched = $mx;
+					break;
+				}
+				$mx++;
+			}
+		}
+		else $this->Title = $this->Filename;
 	}
 }
 
