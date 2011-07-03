5d75efc08e92ed21897467e3824f6674eda3d6d4
Xedecimal <Xedecimal@gmail.com>
2011-07-03 02:49:56 -0700

    Added a feature to sort entries, you can try something like 'obtained' to get the latest movies.

--- modules/search/search.js
+++ modules/search/search.js
@@ -4,4 +4,10 @@ $(function () {
 			window.refreshAll();
 		}, 'html');
 	}});
+
+	$('#movie-sort').autocomplete({minLength: 0, source: function (sender) {
+		$.get('search', {sort: sender.term}, function (data) {
+			window.refreshAll();
+		}, 'html');
+	}});
 });
--- modules/search/search.php
+++ modules/search/search.php
@@ -9,6 +9,7 @@ class ModSearch extends Module
 		if ($_d['q'][0] != 'search') return;
 
 		$_SESSION['search.query'] = Server::GetVar('term');
+		$_SESSION['search.sort'] = Server::GetVar('sort');
 		die();
 	}
 
@@ -21,19 +22,23 @@ class ModSearch extends Module
 		$q = Server::GetVar('search.query');
 		if (!empty($q))
 		{
-			#$_d['movie.cb.query']['match']['fs_title'] = new MongoRegex("/$q/i");
 			$_d['movie.cb.query']['match']['$or'][]['title'] = new MongoRegex("/$q/i");
 			foreach ($_d['search.cb.query'] as $cb)
 				$_d['movie.cb.query']['match'] =
 					array_merge_recursive($_d['movie.cb.query']['match'],
 						call_user_func($cb, $q));
 		}
+
+		$s = Server::GetVar('search.sort');
+		if (!empty($s))
+			$_d['movie.cb.query']['order'] = array($s => -1);
 	}
 
 	function movie_cb_head()
 	{
 		$t = new Template();
 		$t->Set('query', Server::GetVar('search.query', ''));
+		$t->Set('sort', Server::GetVar('search.sort', ''));
 		return $t->ParseFile('modules/search/t.xml');
 	}
 }
