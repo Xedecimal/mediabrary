5fb918e0a2889a22c292c2ec08658e6c40842167
Xedecimal <Xedecimal@gmail.com>
2011-11-09 00:02:24 -0800

    Fixed cover scraping

--- modules/scrape/scrape.js
+++ modules/scrape/scrape.js
@@ -9,15 +9,17 @@ $(function () {
 		$('.covers-'+name).hide(500, function () {$(this).remove()});
 
 		// Have the owning scraper get us some covers for this selection.
-		$.get(window.app_abs+'/'+name+'/covers', {id: $(this).val()}, function (data, stat, d2) {
-			$.each(data.covers, function (ix, url) {
-				var ins = $('<input type="image" src="'+url
-					+'" class="scrape-cover covers-'+data.id+'" width="185"'
-					+' height="275" />');
-				$(ins).appendTo('#div-covers').hide().show(500);
-				//$().append();
-			});
-		}, 'json');
+		$.get(window.app_abs+'/'+name+'/covers', {id: $(this).val()},
+			function (data, stat, d2) {
+				$.each(data.covers, function (ix, url) {
+					var ins = $('<input type="image" src="'+url
+						+'" class="scrape-cover covers-'+data.id+'" width="185"'
+						+' height="275" />');
+					$(ins).appendTo('#div-covers').hide().show(500);
+					//$().append();
+				});
+			}, 'json'
+		);
 	});
 
 	$('.scrape-cover').live('click', function () {
