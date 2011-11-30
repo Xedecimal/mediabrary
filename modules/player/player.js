0c3f91e9ec1d08ed1a0459af7a17e744d3961823
Xedecimal <Xedecimal@gmail.com>
2011-11-29 22:17:06 -0800

    Fixed the retarded vlc remote control shoddy path handling.

--- modules/player/player.js
+++ modules/player/player.js
@@ -12,8 +12,9 @@ $(function () {
 	$('#a-vlc').live('click', function () {
 		var tpath = $(this).attr('href');
 
-		var url = 'http://'+remote_ip+':8080'
-		url += '/requests/status.xml?command=in_play&input='+tpath;
+		var url = 'http://'+remote_ip+':8080/requests/status.xml';
+		url += '?command=in_play';
+		url += '&input='+tpath.replace(/'/g, "\\'");
 
 		// Attempt in javascript to request through client's vlc player.
 		$.get(url);
