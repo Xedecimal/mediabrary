0850a1679569a49996ee1102c5c3ea1a1c3671c4
Xedecimal <Xedecimal@Xedecimal-PC.bad.ass>
2012-03-15 01:14:27 -0700

    Fixed some js issues when multiple detail dialogs were opened and closed.

--- modules/music/music.js
+++ modules/music/music.js
@@ -2,12 +2,14 @@ $(function () {
 	$('#dialog-music');
 
 	$('.a-music-item').live('click', function () {
-		$('<div />').dialog({
+		$('<div id="detail-dialog" />').dialog({
 			width: '80%',
 			height: 500,
 			position: 'top',
-			title: 'Artist Details'})
-			.load($(this).attr('href'));
+			title: 'Artist Details',
+
+			close: function () { $('#detail-dialog').remove(); }
+		}).load($(this).attr('href'));
 
 		return false;
 	});
