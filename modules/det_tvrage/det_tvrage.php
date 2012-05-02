faaa19a9304120436d9cb73af2ccc922da43e2af
Nicholas Libby <Xedecimal@gmail.com>
2012-05-02 16:49:35 -0700

    Lets just say TVRage is well on it's way to being back in business.

--- modules/det_tvrage/det_tvrage.php
+++ modules/det_tvrage/det_tvrage.php
@@ -15,8 +15,7 @@ class TVRage extends Module implements Scraper
 	{
 		global $_d;
 
-		# @TODO: Bring this back when others work better.
-		#$_d['tv.cb.check.series'] = array(&$this, 'cb_tv_check');
+		$_d['tv.cb.check.series']['TVRage'] = array(&$this, 'cb_tv_check_series');
 	}
 
 	# Scraper implementation
@@ -31,20 +30,15 @@ class TVRage extends Module implements Scraper
 
 	function GetCovers($item) {}
 
-	static function GetSID($path, $title)
+	static function GetSID($path, $title = null)
 	{
 		$tvse = new TVSeriesEntry($path);
 
 		if (empty($title)) $title = $tvse->Title;
 
 		$url = TVRage::_tvrage_find.rawurlencode($title);
-		$ctx = stream_context_create(array('http' => array('timeout' => 5)));
-		$xml = @file_get_contents($url, false, $ctx);
-		if (empty($xml))
-		{
-			$ret[]['title'] = 'Timed Out.';
-			return $ret;
-		}
+		$xml = file_get_contents($url);
+		if (empty($xml)) return null;
 		$sx = simplexml_load_string($xml);
 
 		foreach ($sx->show as $s)
@@ -62,75 +56,205 @@ class TVRage extends Module implements Scraper
 		return $items;
 	}
 
-	static function GetXML($series, $download = false)
+	static function GetData($series, $download = false)
 	{
 		global $_d;
 
-		$sid = TVRage::GetSID($series);
-		if ($sid == -1) {
-			echo "Could not locate this series $series";
-			return null;
-		}
-
-		$infoloc = "$series/.tvrage.series.xml";
+		$infoloc = $series->Path."/.tvrage_cache.json";
 		if ($download || !file_exists($infoloc))
 		{
+			$sids = TVRage::GetSID($series->Title);
+
+			if (empty($sids))
+			{
+				echo "Could not locate this series {$series->Title}";
+				return null;
+			}
+
+			$sid = $sids[0]['id'];
+
 			$url = TVRage::_tvrage_list.$sid;
-			$out = file_get_contents($url);
-			@file_put_contents($infoloc, $out);
+			$out = json_encode(Arr::FromXML(file_get_contents($url)));
+			file_put_contents($infoloc, $out);
 		}
 
-		return simplexml_load_string(file_get_contents($infoloc));
+		return json_decode(file_get_contents($infoloc), true);
 	}
 
 	static function GetInfo($series)
 	{
 		$ret = array();
 
-		$sx = $this->GetXML($series);
-		$ret['series'] = trim((string)$sx->name);
-		foreach ($sx->xpath('//Season') as $s)
+		$json = TVRage::GetData($series);
+
+		if (empty($json)) die();
+
+		$ret['series'] = trim($json['name']);
+
+		# Single season, make it uniform.
+		if ($json['totalseasons'] == 1)
+			$json['Episodelist']['Season'] = array($json['Episodelist']['Season']);
+
+		foreach ($json['Episodelist']['Season'] as $s)
 		{
-			$sn = (int)$s->attributes()->no;
-			foreach ($s->xpath('episode') as $ep)
+			if (empty($s['episode'])) continue;
+
+			if (array_key_exists('epnum', $s['episode']))
+				$s['episode'] = array($s['episode']);
+
+			foreach ($s['episode'] as $ep)
 			{
-				// Blank date.
-				if ($ep->airdate != '0000-00-00' && !preg_match('/\d{4}-00-00/', $ep->airdate))
-					$eout['aired'] = Database::MyDateTimestamp($ep->airdate);
-				if (!empty($ep->title)) $eout['title'] = (string)$ep->title;
-				$eout['links']['TVRage'] = (string)$ep->link;
-				$en = (int)$ep->seasonnum;
-				$ret['eps'][$sn][$en] = $eout;
+				foreach (array_keys($ep) as $k) if (empty($ep[$k])) unset($ep[$k]);
+
+				$sn = $ep['seasonnum'] = (int)$ep['seasonnum'];
+				$ep['epnum'] = (int)$ep['epnum'];
+				$en = (int)$ep['seasonnum'];
+				if (!empty($ep['airdate'])) $ep['airdate'] =
+					Database::MyDateTimestamp($ep['airdate']);
+				if (!empty($ep['rating'])) $ep['rating'] = (float)$ep['rating'];
+				$ep['details']['TVRage'] = $ep;
+				$ret['eps'][$sn][$en] = $ep;
 			}
 		}
 
 		return $ret;
 	}
 
-	public function CanAuto() {
+	public function CanAuto() {}
+	public function Details($id) {}
+	public function GetDetails($t, $g, $a) {}
+	public function GetName() {}
+	public function Scrape(&$item, $id = null) {}
 
-	}
+	# Checking
 
-	public function Details($id) {
-
-	}
-
-	public function GetDetails($t, $g, $a) {
-
-	}
-
-	public function GetName() {
-
-	}
-
-	public function Scrape(&$item, $id = null) { }
-
-	# Callbacks
-
-	function cb_tv_check_series($series, &$msgs)
+	function cb_tv_check_series($series)
 	{
+		$tvreps = TVRage::GetInfo($series);
+
+		if (empty($tvreps)) return;
+
+		$this->CheckSeriesFilename($series, $tvreps);
+
+		foreach ($tvreps['eps'] as $s => $eps)
+		{
+			foreach ($eps as $e => $ep)
+			{
+				# No database entry for this episode.
+				if (empty($series->ds[$s][$e]))
+				{
+					$tve = new TVEpisodeEntry(null);
+					$tve->Data['details'][$this->Name] = $ep['details'][$this->Name];
+					if (!empty($ep['airdate']))
+						$tve->Data['released'] = new MongoDate($ep['airdate']);
+					if (isset($ep['title'])) $tve->Data['title'] = $ep['title'];
+					$tve->Data['type'] = 'tv-episode';
+					$tve->Data['series'] = $series->Title;
+					$tve->Data['season'] = $s;
+					$tve->Data['episode'] = $e;
+					$tve->Data['index'] = sprintf('S%02dE%02d', $s, $e);
+					$tve->Title = $ep['title'];
+					$tve->Data['parent'] = $series->Data['_id'];
+					$dbep = $tve->Data;
+					$tve->SaveDS(true);
+					ModCheck::Out("Added missing episode data for {$series->Title} of $s $e from {$this->Name}.");
+				}
+				else $dbep = $series->ds[$s][$e];
+
+				# Check series.
+				$this->CheckDetails($dbep, $ep);
+
+				# This tv-episode is in the filesystem.
+				if (!empty($dbep['path']))
+				{
+					# Check filename.
+					$this->CheckFilename($tvdbeps, $dbep, $ep);
+				}
+			}
+		}
+	}
+
+	function CheckSeriesFilename(&$series, &$data)
+	{
+		$src = dirname($series->Path).'/'.basename(realpath($series->Path));
+		$title = MediaLibrary::CleanTitleForFile($data['series']);
+		$dst = dirname($series->Path).'/'.$title;
+		$url = Module::L('tv/rename?path='.urlencode($src).'&amp;target='.urlencode($dst));
+		if ($src != $dst) ModCheck::Out("<a href=\"$url\" class=\"a-fix button\">Fix</a> Series '$src' should be '$dst'");
+	}
+
+	function CheckFilename(&$series, $ep, $dvdbep)
+	{
+		global $_d;
+
+		if (empty($ep['details'][$this->Name])) return;
+
+		$epname = @$ep['details'][$this->Name]['title'];
+
+		if (!empty($epname))
+			$epname = MediaLibrary::CleanTitleForFile($epname, false);
+
+		$sn = MediaLibrary::CleanTitleForFile($series['Series']['SeriesName']);
+
+		$preg = '@([^/]+)/('.preg_quote($sn,'@')
+			.') - S([0-9]{2,3})E([0-9]{2,3}) - '
+			.preg_quote($epname, '@').'\.([^.]+)$@';
+
+		# <series> / <series> - S<season>E<episode> - <title>.avi
+		if (!preg_match($preg, $ep['path']))
+		{
+			$dir = dirname($ep['path']);
+			$ext = File::ext($ep['path']);
+			$fns = sprintf('%02d', $ep['season']);
+			$fne = sprintf('%02d', $ep['episode']);
+			$fname = "$sn - S{$fns}E{$fne} - {$epname}";
+			$url = $_d['app_abs'].'/tv/rename?path='.urlencode($ep['path']).'&amp;target='.urlencode("$dir/$fname.$ext");
+			TV::OutErr("<a href=\"$url\" class=\"a-fix button\">Fix</a> File {$ep['path']} has invalid name, should be \"$dir/$fname.$ext\"", $ep);
+			return false;
+		}
+	}
+
+	function CheckDetails($ep, $tvrep)
+	{
+		global $_d;
+
+		# No data to reference
+		if (!isset($ep['details'][$this->Name]))
+		{
+			$ep['details'][$this->Name] = $tvrep;
+			$_d['entry.ds']->Save($ep);
+			#$nep->SaveDS();
+			ModCheck::Out("Adding metadata for {$ep['series']} {$ep['season']}x{$ep['episode']}", $ep);
+		}
+
+		# Release Date
+		if (!empty($ep['details'][$this->Name]['airdate']))
+		{
+			if (empty($ep['released']))
+			{
+				$ep['released'] = $ep['details'][$this->Name]['airdate'];
+				//$nep->SaveDS();
+				ModCheck::Out("TODO: NOT Filled release date for {$ep['series']} {$ep['season']}x{$ep['episode']}");
+			}
+
+			# Already aired, missing.
+			if (empty($ep['path'])
+			&& !empty($ep['season'])
+			&& !empty($ep['episode'])
+			&& strtotime($ep['details'][$this->Name]['airdate']) < time())
+			{
+				$title = @$ep['details'][$this->Name]['title'];
+				if (empty($title)) $title = "[Yet unknown]";
+
+				ModCheck::Out("Missing episode {$ep['series']}"
+					." S{$ep['season']}E{$ep['episode']} {$title}"
+					." on {$ep['details'][$this->Name]['airdate']}");
+			}
+		}
 	}
 }
+
+Module::Register('TVRage');
 
 Scrape::Reg('tv', 'TVRage');
 Scrape::Reg('tv-series', 'TVRage');
