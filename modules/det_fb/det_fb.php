a5b9dca734b84179d4f5850bcc3dc2ff43522ba0
Nicholas Libby <Xedecimal@gmail.com>
2012-05-02 17:42:26 -0700

    Added some examples and some basic stub structure for the start of the Freebase system.

--- modules/det_fb/det_fb.php
+++ modules/det_fb/det_fb.php
@@ -1,5 +1,33 @@
 <?php
 
-# http://api.freebase.com/api/service/search?query=Hellraiser&type=/film/film&indent=1
+# http://www.freebase.com/queryeditor
+# https://www.googleapis.com/freebase/v1/search?query=American%20Dad!&type=/tv/tv_program
 
-?>
+/*
+Single TV series, missing data still.
+{
+  "type":     "/tv/tv_program",
+  "id": "/en/american_dad",
+  "name":     null,
+  "episodes": [{}],
+  "seasons": [{}],
+  "/common/topic/image": [{}]
+}
+ */
+
+class Freebase
+{
+	const URL_SEARCH = 'https://www.googleapis.com/freebase/v1/search?query=%s&type=%s';
+
+	function Search($query, $type)
+	{
+		return file_get_contents(sprintf(Freebase::URL_SEARCH,
+			urlencode($query),
+			urlencode($type)));
+	}
+
+	function Query()
+	{
+		file_get_contents();
+	}
+}
